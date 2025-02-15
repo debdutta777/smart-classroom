c5a5a22162d5866203dd34b5fe5f6728ba5b4071
debdutta777 <159032315+debdutta777@users.noreply.github.com>
2025-02-15 12:55:06 +0530

    all right

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Container, AppBar, Toolbar, Typography, Button } from "@mui/material";
+
+function Home() {
+  return <Typography variant="h4">Welcome to Smart Classroom</Typography>;
+}
+
+function Login() {
+  return <Typography variant="h4">Login Page</Typography>;
+}
+
+function Register() {
+  return <Typography variant="h4">Register Page</Typography>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Smart Classroom
+          </Typography>
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/login">Login</Button>
+          <Button color="inherit" component={Link} to="/register">Register</Button>
+        </Toolbar>
+      </AppBar>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Container>
   );
 }
 
