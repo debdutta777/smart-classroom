ef2bd1db84004d24960ecee9a785e323c1423551
debdutta777 <159032315+debdutta777@users.noreply.github.com>
2025-02-15 12:43:52 +0530

    ch

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+
+const Home = () => {
+  return (
+    <Container>
+      <Typography variant="h3">Welcome to Smart Classroom</Typography>
+    </Container>
+  );
+};
+
+export default Home;
